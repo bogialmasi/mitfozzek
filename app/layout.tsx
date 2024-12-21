1337fda899d5481df717f0d0598f53ad3a04b8e2
bogialmasi <boglarkaalmasi@gmail.com>
2024-12-21 17:50:23 +0100

    removed unnecessary link

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,14 +48,7 @@ export default function RootLayout({
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Mitfozzek.hu 2024</span>
-              </Link>
+              <span className="text-default-600">Mitfozzek.hu 2024</span>
             </footer>
           </div>
         </Providers>
