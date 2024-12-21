e21da2686e801e0a95a9a901f6f6e9448d465f9d
bogialmasi <boglarkaalmasi@gmail.com>
2024-12-21 17:48:04 +0100

    Added MyDropdown component to search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,122 +1,54 @@
 'use client'
 import React from "react";
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import { Button, Form, Input } from "@nextui-org/react";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
+import { MyDropdown } from "@/components/dropdown";
+import { Examples } from "@/config/example";
 
 export default function SearchPage() {
-  // Előételek, Főételek, Desszertek stb. list
-  const [selectedKeysDish, setSelectedKeysDish] = React.useState(new Set());
 
-  const selectedValueDish = React.useMemo(() => {
-    const selectedArray = Array.from(selectedKeysDish);
-    return selectedArray.length > 0
-      ? selectedArray.join(", ").replaceAll("_", " ")
-      : "Bármilyen";  // Fallback if no selection
-  }, [selectedKeysDish]);
-  
-  const handleSelectChangeDish = (keys) => {
-    setSelectedKeysDish(new Set(keys));
-  };
+  // State for selections in each dropdown
+  const [selectedKeysDish, setSelectedKeysDish] = React.useState<Set<string>>(new Set());
+  const [selectedKeysSensitivity, setSelectedKeysSensitivity] = React.useState<Set<string>>(new Set());
+  const [selectedKeysRestriction, setSelectedKeysRestriction] = React.useState<Set<string>>(new Set());
 
-  // Vegan, Vegeratian
-  const [selectedKeysMeat, setSelectedKeysMeat] = React.useState(new Set(["Vegán, Vegetariánus?"]));
-  const selectedValueMeat = React.useMemo(
-    () => Array.from(selectedKeysMeat).join(", ").replace(/_/g, ""),
-    [selectedKeysMeat],
-  );
-
-  // Food Sensitivity
-  const [selectedKeysSensitivity, setSelectedKeysSensitivity] = React.useState(new Set());
-
-  const selectedValueSensitivity = React.useMemo(() => {
-    const selectedArray = Array.from(selectedKeysSensitivity);
-    return selectedArray.length > 0
-      ? selectedArray.join(", ").replaceAll("_", " ")
-      : "Nincs ételérzékenység";  // Fallback if no selection
-  }, [selectedKeysSensitivity]);
-  
-  const handleSelectChangeSensitivity = (keys) => {
-    setSelectedKeysSensitivity(new Set(keys));
-  };
 
   return (
     <Form className="">
-      <div className="inline-block max-w-xl text-center justify-center gap-6">
+      <div className="inline-block max-w-xl text-center justify-center py-45 md:py-8">
         <h1 className={title()}>Receptek keresése</h1>
       </div>
       <Input
-        className="form-control input gap-6"
+        className="form-control input"
         labelPlacement="outside"
         name="search"
         placeholder="Keressen rá receptek címére, vagy hozzávalóra..."
         type="text"
         variant="bordered"
       />
-
-      <Dropdown>
-        <DropdownTrigger>
-          <Button className="capitalize" variant="bordered">
-            {selectedValueDish}
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu
-          disallowEmptySelection={false}
-          closeOnSelect={false}
-          selectedKeys={selectedKeysDish}
-          selectionMode="multiple"
-          variant="flat"
-          onSelectionChange={handleSelectChangeDish}
-        >
-          <DropdownItem key="előételek">Előételek</DropdownItem>
-          <DropdownItem key="levesek">Levesek</DropdownItem>
-          <DropdownItem key="főételek">Főételek</DropdownItem>
-          <DropdownItem key="saláták">Saláták</DropdownItem>
-          <DropdownItem key="desszertek">Desszertek</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-
-      <Dropdown>
-        <DropdownTrigger>
-          <Button className="capitalize" variant="bordered">
-            {selectedValueSensitivity}
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu
-          disallowEmptySelection={false}
-          closeOnSelect={false}
-          selectedKeys={selectedKeysSensitivity}
-          selectionMode="multiple"
-          variant="flat"
-          onSelectionChange={handleSelectChangeSensitivity}
-        >
-          <DropdownItem key="Gluténérzékenység">Gluténérzékenység</DropdownItem>
-          <DropdownItem key="Laktózérzékenység">Laktózérzékenység</DropdownItem>
-          <DropdownItem key="Diabetikus">Diabetikus</DropdownItem>
-          <DropdownItem key="Mogyoróallergia">Mogyoróallergia</DropdownItem>
-          <DropdownItem key="Tojásallergia">Tojásallergia</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-
-      <Dropdown>
-        <DropdownTrigger>
-          <Button className="capitalize" variant="bordered">
-            {selectedValueMeat}
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu
-          disallowEmptySelection={false}
-          selectedKeys={selectedKeysMeat}
-          selectionMode="single"
-          variant="flat"
-          onSelectionChange={setSelectedKeysMeat}
-        >
-          <DropdownItem key="húsos">Húsos fogások</DropdownItem>
-          <DropdownItem key="vegetariánus">Vegetariánus fogások</DropdownItem>
-          <DropdownItem key="vegán">Vegán ételek</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+      <div className="inline-block max-w-xl w-max text-left justify-center gap-2 py-2">
+      <p className="text-sm py-2">Étel típusa:</p>
+      <MyDropdown
+        list={Examples.dishTypes} // Pass the dishTypes list
+        selectedKeys={selectedKeysDish}
+        onSelectionChange={(keys) => setSelectedKeysDish(new Set(keys))}
+      />
+      <p className="text-sm py-2">Ételérzékenységek:</p>
+      <MyDropdown
+        list={Examples.foodSensitivities} // Pass the foodSensitivities list
+        selectedKeys={selectedKeysSensitivity}
+        onSelectionChange={(keys) => setSelectedKeysSensitivity(new Set(keys))}
+      />
+      <p className="text-sm py-2">Étrendbeli megkötések:</p>
+      <MyDropdown
+        list={Examples.dietaryRestrictions} // Pass the dietaryRestrictions list
+        selectedKeys={selectedKeysRestriction}
+        onSelectionChange={(keys) => setSelectedKeysRestriction(new Set(keys))}
+      />
+      </div>
+      <div>
       <Button type="submit" className="btn btn-primary mb-2 justify-center">Keresés</Button>
+      </div>
     </Form>
   );
 }
