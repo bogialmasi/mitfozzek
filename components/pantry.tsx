1fbc276a54ad21d34f43ad8c5109fadf08625cc4
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-06 17:31:23 +0100

    pantry component

--- components/pantry.tsx
+++ components/pantry.tsx
@@ -1,3 +1,4 @@
+'use client'
 /*
 
 this will be a list within /profile, where the user can add and remove ingredients (from the ingredients list)
@@ -6,4 +7,43 @@ the DELETE and CHANGE QUANTITY should work as an options menu when clicking on t
 
 */
 
-export const Pantry = () => {};
+import { subtitle } from "@/components/primitives";
+import { Button } from "@nextui-org/react";
+import React from "react";
+
+export const Pantry = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-gray-200 rounded-lg shadow-lg w-full max-w-lg h-[90vh] flex flex-col">
+                <div className="p-4 border-b border-gray-300 justify-center">
+                    <div className={subtitle({ class: "mt-4" })}>
+                        Spájzom
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Keresés a hozzávalók között..."
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    />
+                </div>
+
+                <div className="flex-1 overflow-y-auto p-4">
+                    <ul className="space-y-2">
+                        {Array.from({ length: 26 }, (_, i) => (
+                            <li key={i} className="p-2 bg-white rounded-md shadow-sm">
+                                {String.fromCharCode(65 + i)}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2">
+                    <div className="p-4 border-t border-gray-300 flex justify-center">
+                        <Button type="submit">Hozzáadás</Button>
+                    </div>
+                    <div className="p-4 border-t border-gray-300 flex justify-center">
+                        <Button type="submit">Módosítás</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
