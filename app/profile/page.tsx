d23aeede676965a763e2d5580513c3f0ba3b419c
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-06 15:47:41 +0100

    ui for profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,49 @@
+'use client'
 import { title, subtitle } from "@/components/primitives";
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
 
 export default function ProfilePage() {
   return (
-    <div>
-      <h1 className={title()}>Profilom</h1>
-      <div className={subtitle({ class: "mt-4" })}>
-        Ide kerül profilleírás, adatok, settings,
-        Kedvenc receptek listája, 
-        Innen lehet menni spájzom-ba
-        Bevásárlólistákat itt lehet megtalálni, azokat akár kinyomtatni, törölni
+    <section className="flex flex-col gap-6 py-8 md:py-10">
+      <div className="justify-right gap-6"><h1 className={title()}>Profilom</h1></div>
+      <div className="flex flex-wrap justify-center gap-4">
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.profileMenuItems.addRecipe}
+        >
+          Recept hozzáadása
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.profileMenuItems.myRecipes}
+        >
+          Receptjeim
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.profileMenuItems.favRecipes}
+        >
+          Kedvenc receptjeim
+        </Link>
+        <div className={subtitle({ class: "mt-4" })}>
+          <h3>Hello, i am XY ZX, i am 30 years old and i have found my passion in cooking. I am eager to try any new and fun recipes, as long as they are vegan</h3>
+        </div>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.profileMenuItems.modifyProfile}
+        >
+          Profilom módosítása
+        </Link>
+        <div className={subtitle({ class: "mt-4" })}>
+        <h3>Keresési feltéteim: <br/> <Link> Módosítás </Link></h3>
       </div>
-    </div>
+      </div>
+    </section>
   );
 }
