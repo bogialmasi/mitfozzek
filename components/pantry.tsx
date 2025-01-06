aa4c6281fe77569370ae2fbe5671f1fef7e923c2
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-06 15:47:11 +0100

    pantry will be the component showing what ingredients the user has listed

---
+++ components/pantry.tsx
@@ -0,0 +1,9 @@
+/*
+
+this will be a list within /profile, where the user can add and remove ingredients (from the ingredients list)
+the ADD button should have a popup (modal) on which the user can select which ingredients to add and write how many of that
+the DELETE and CHANGE QUANTITY should work as an options menu when clicking on the item.
+
+*/
+
+export const Pantry = () => {};
