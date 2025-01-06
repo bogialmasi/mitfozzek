919688fd07963962e74f81d792b8ae2d24e2ae6e
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-06 17:31:45 +0100

    profile ui

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,14 +1,29 @@
+'use client'
+import { Pantry } from "@/components/pantry";
+import { usePathname } from "next/navigation";
+
 export default function ProfileLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <section className="flex flex-col gap-4">
-        <div className="inline-block max-w-lg">
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  const pathname = usePathname();
+  const isNotSubRoute = pathname === '/profile' || pathname === '/profile/';
+
+  if(isNotSubRoute) { return (
+    <section className="flex flex-col gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 h-screen gap-x-6">
+          {children}
+          <Pantry />
+        </div>
+      </section>
+  )}
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
           {children}
         </div>
       </section>
-    );
-  }
-  
+  )
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,44 +6,48 @@ import { siteConfig } from "@/config/site";
 
 export default function ProfilePage() {
   return (
-    <section className="flex flex-col gap-6 py-8 md:py-10">
-      <div className="justify-right gap-6"><h1 className={title()}>Profilom</h1></div>
-      <div className="flex flex-wrap justify-center gap-4">
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.profileMenuItems.addRecipe}
-        >
-          Recept hozzáadása
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.profileMenuItems.myRecipes}
-        >
-          Receptjeim
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.profileMenuItems.favRecipes}
-        >
-          Kedvenc receptjeim
-        </Link>
-        <div className={subtitle({ class: "mt-4" })}>
-          <h3>Hello, i am XY ZX, i am 30 years old and i have found my passion in cooking. I am eager to try any new and fun recipes, as long as they are vegan</h3>
+    <div>
+    <div className="grid grid-flow-col auto-cols-max md:auto-cols-min grid-cols-1">
+      <section className="flex flex-col gap-6 py-8 md:py-10">
+        <div className="justify-right gap-6"><h1 className={title()}>Profilom</h1></div>
+        <div className="flex flex-wrap justify-center gap-4">
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.profileMenuItems.addRecipe}
+          >
+            Recept hozzáadása
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.profileMenuItems.myRecipes}
+          >
+            Receptjeim
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.profileMenuItems.favRecipes}
+          >
+            Kedvenc receptjeim
+          </Link>
+          <div className={subtitle({ class: "mt-4" })}>
+            <h3>Hát Izsák vagyok, hobbiszakács, szeretek új és izgalmas recepteket kipróbálni, szeretem a keleti ízeket, és érdekel a vegán életmód is</h3>
+          </div>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.profileMenuItems.modifyProfile}
+          >
+            Profilom módosítása
+          </Link>
+          <div className={subtitle({ class: "mt-4" })}>
+            <h3>Keresési feltéteim: <br /> <Link> Módosítás </Link></h3>
+          </div>
         </div>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.profileMenuItems.modifyProfile}
-        >
-          Profilom módosítása
-        </Link>
-        <div className={subtitle({ class: "mt-4" })}>
-        <h3>Keresési feltéteim: <br/> <Link> Módosítás </Link></h3>
-      </div>
-      </div>
-    </section>
+      </section>
+    </div>
+    </div>
   );
 }
