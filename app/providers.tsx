29e006257ac98cb145ce3595b04c852171924c85
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-08 19:20:10 +0100

    dark/light mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,31 @@
-// app/providers.tsx
 "use client";
 
-import {useRouter} from "next/navigation";
-import {NextUIProvider} from "@nextui-org/react";
+import type { ThemeProviderProps } from "next-themes";
 
-// Only if using TypeScript
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
 declare module "@react-types/shared" {
   interface RouterConfig {
-    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>["push"]>[1]>;
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
   }
 }
 
-export function Providers({children}: {children: React.ReactNode}) {
+export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </NextUIProvider>
+  );
 }
