faeba8034320f4ccf5d237fe4c28377f55127ddd
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-08 16:09:45 +0100

    db queries

---
+++ app/api/data/route.tsx
@@ -0,0 +1,50 @@
+import pool from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const type = searchParams.get('type'); // the value of the type parameter from the URL
+
+  if (!type) {
+    return NextResponse.json(
+      { error: 'Hiányzó paraméter' },
+      { status: 400 }
+    );
+  }
+
+  // SQL query based on the type parameter
+  let query = '';
+  switch (type) {
+    case 'ingredients':
+      query = 'SELECT ingredient_id AS "key", ingredient_name AS "value" FROM ingredients;';
+      break;
+    case 'dish_type':
+      query = 'SELECT dishtype_id AS "key", dishtype_name AS "value" FROM dish_type;';
+      break;
+    case 'dish_category':
+      query = 'SELECT category_id AS "key", category_name AS "value" FROM dish_category;';
+      break;
+    default:
+      return NextResponse.json(
+        { error: 'Érvénytelen paraméter' },
+        { status: 400 }
+      );
+    }
+
+  try{
+    const [rows] = await pool.query(query);
+    return NextResponse.json(rows); // fetched data is returned
+  } catch (error) {
+    console.error('Adatbázis hívás hiba:', error);
+    return NextResponse.json(
+      { error: 'Adatbázis hiba' },
+      { status: 500 }
+    );
+  }
+
+
+}
+
+export async function POST() {
+  return NextResponse.json({ message: 'This is a POST method' });
+}
