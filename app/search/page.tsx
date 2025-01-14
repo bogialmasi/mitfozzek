7a17ac87bbd360ff2e4d88cf9c6bf72cf0a804dc
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-14 14:22:56 +0100

    search page redirects to results

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,36 +3,33 @@ import React from 'react';
 import { MySearch } from '@/components/search';
 import { Button, Form, Input } from '@nextui-org/react';
 import { title } from '@/components/primitives';
+import { useRouter } from 'next/navigation';
 
 export default function SearchPage() {
+  const router = useRouter();
   // Function to handle search submission
   const handleSearch = (filters: {
     ingredients: number[];
     dishType: number[];
     dishCategory: number[];
   }) => {
-    console.log('Search Filters:', filters);
-    // Perform your search logic or API call here
+
+    console.log('Filterek:', filters);
+    const params = new URLSearchParams();
+    filters.ingredients.forEach((id) => params.append('ingredients', id.toString()));
+    filters.dishType.forEach((id) => params.append('dishType', id.toString()));
+    filters.dishCategory.forEach((id) => params.append('dishCategory', id.toString()));
+    
+    // Navigate to the results page with query parameters
+    router.push(`/search/results?${params.toString()}`);
   };
 
   return (
-    <Form className="">
-      <div className="inline-block max-w-xl text-center justify-center">
+    <section className="flex flex-col items-center justify-center">
+      <div className="w-full max-w-xl text-center justify-center overflow-visible ">
         <h1 className={title()}>Receptek keresése</h1>
+        <MySearch onSearch={handleSearch} />
       </div>
-      <Input
-        className="form-control input py-4"
-        labelPlacement="outside"
-        name="search"
-        placeholder="Keressen rá receptek címére..."
-        type="text"
-        variant="bordered"
-      />
-      <div className="inline-block w-max text-left justify-center py-2">
-        <div className="container mx-auto p-4">
-          <MySearch onSearch={handleSearch} />
-        </div>
-      </div>
-    </Form>
+    </section>
   );
 }
