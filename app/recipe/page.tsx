73cc2ef749835850e9d08d1593f65aa1dcb30ef9
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-16 16:21:59 +0100

    recipe page displaying one recipe

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,14 +1,76 @@
-import { title, subtitle } from "@/components/primitives";
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { title } from '@/components/primitives';
+
+// Recipe interface
+interface Recipe {
+  recipe_id: number;
+  recipe_name: string;
+  recipe_description: string;
+  recipe_time: string;
+  recipe_headcount: number;
+  source_user_id: number;
+  ingredients: Ingredient[];
+}
+
+interface Ingredient {
+  id: number;
+  name: string;
+}
 
 export default function RecipePage() {
+  const searchParams = useSearchParams();
+  const [resultRecipe, setResultRecipe] = useState<Recipe | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      setLoading(true);
+      setError(null);
+
+      const id = searchParams.get('id');
+      console.log('Query Parameter ID:', id);
+
+      if (!id) {
+        setError('No ID provided in the query parameters');
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(`/api/search?id=${id}`); // Fetch using the query parameter
+        if (!response.ok) {
+          throw new Error('Recept lekérése sikertelen');
+        }
+        const data = await response.json();
+        console.log('Recept:', data);
+        setResultRecipe(data);
+      } catch (error) {
+        setError((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchResults();
+  }, [searchParams]);
+
+  if (loading) return <p>Recept betöltése...</p>;
+  if (error) return <p>Hiba: {error}</p>;
+  if (!resultRecipe) {
+    return <p>No recipe matches the search criteria :(</p>;
+  }
+
   return (
-    <div>
-      <h1 className={title()}>Recept</h1>
-      <div className={subtitle({ class: "mt-4" })}>
-        Egy adott recept leírása, összetevőinek listája, lépések leírása
-        Shopping list nyomtatása, kedvencekhez hozzáadás, 
-        Spájzban lévő összetevők összehasonlítása, hogy van-e (tuti külön komponens)
+    <section className="flex flex-col items-center justify-center">
+      <div className="w-full max-w-xl text-center justify-center overflow-visible">
+        <h1 className={title()}>{resultRecipe.recipe_name}</h1>
+        <p>{resultRecipe.recipe_description}</p>
+        <p>Idő: {resultRecipe.recipe_time || 'Nincs időtartam'}</p>
       </div>
-    </div>
+    </section>
   );
 }
