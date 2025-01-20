01b32b948d0bf66c667bf73313f511b312adaf67
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-20 15:23:02 +0100

    Back To Top button

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { MyBackToTopButton } from "@/components/backtotop";
+
+/**
+ * <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+ */
 
 export const metadata: Metadata = {
   title: {
@@ -44,8 +49,9 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col min-h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="flex-grow">
               {children}
+              <MyBackToTopButton/>
             </main>
             <footer className="w-full flex items-center justify-center py-3">
               <Footer/>
---
+++ components/backtotop.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button } from "@nextui-org/react";
+import { ArrowUpIcon } from "./icons";
+import { button as buttonStyles } from '@nextui-org/theme';
+
+export const MyBackToTopButton: React.FC = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsVisible(window.scrollY > 200); // Show the button when scrolled more than 200px
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div
+            className={`fixed bottom-4 right-4 transition-opacity duration-500 ${isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+        >
+            <Button
+                color="primary"
+                className={buttonStyles({
+                    color: "primary",
+                    radius: "full",
+                    variant: "shadow",
+                })}
+                onPress={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+                <ArrowUpIcon />
+            </Button>
+        </div>
+    );
+};
