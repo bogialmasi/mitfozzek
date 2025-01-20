a64070a947a890eb1070a6da187b88d2a068871c
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-20 15:26:23 +0100

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,10 +3,10 @@ export const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-transparent text-current py-4 mt-10">
-            <div className="container mx-auto text-center">
-                <p className="text-sm mt-2">Mitfőzzek &copy; {currentYear}</p>
-            </div>
-        </footer>
+<footer className="bg-transparent text-current py-4 mt-10">
+      <div className="container mx-auto text-center">
+        <p className="text-sm mt-2">Mitfőzzek &copy; {currentYear}</p>
+      </div>
+    </footer>
     );
 };
