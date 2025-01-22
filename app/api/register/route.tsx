1d36b65ec22c291af9868cf72a597119255b3b76
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-22 18:39:48 +0100

    password hashing

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import argon2 from 'argon2';
 import pool from '@/lib/db';
-
+// https://argon2.online/
 export async function POST(req: NextRequest) {
   try {
     const { username, password, email } = await req.json();
@@ -23,8 +23,23 @@ export async function POST(req: NextRequest) {
     }
 
     // Hash the password using Argon2
-    const hashedPassword = await argon2.hash(password);
-
+    const hashPassword = async (password: string) => {
+      try {
+        // Hash the password using Argon2 (this automatically salts the password)
+        const hashedPassword = await argon2.hash(password, {
+          type: argon2.argon2i, // Argon2i - provides resistance to side-channel attacks
+          memoryCost: 2 ** 16, // Memory cost: how much memory to use for hashing (higher is more secure)
+          timeCost: 4, // Time cost: number of iterations (higher is more secure)
+          parallelism: 1, // Parallelism: how many threads to use (higher increases security)
+        });
+    
+        return hashedPassword;
+      } catch (err) {
+        console.error("Error hashing password:", err);
+        throw new Error("Password hashing failed");
+      }
+    };
+    const hashedPassword = hashPassword(password)
     // Insert new user into the database
     const [result] = await pool.query(
       'INSERT INTO users (username, password, email) VALUES (?, ?, ?)',
