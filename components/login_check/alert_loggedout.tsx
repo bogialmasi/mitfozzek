f04ee1115ff887eab602a93e751a2418246e5d9a
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-27 14:27:52 +0100

    logout alert is handled in one component, not two

--- components/login_check/alert_loggedout.tsx
+++ components/login_check/alert_loggedout.tsx
@@ -1,17 +1,41 @@
-import { Alert, Button } from "@heroui/react";
+'use client';
 
-export const MyAlert = () => {
-    return (
-        <Alert
-            color="warning"
-            title="A bejelentkezés ideje lejárt"
-            description="Jelentkezzen be újra"
-            endContent={
-                <Button color="warning" size="sm" variant="flat">
-                    <a href="/login">Bejelentkezés</a>
-                </Button>
-            }
-            variant="faded"
-        />
-    );
-}
+import { useAuthentication } from '@/app/context/authenticationContext';
+import { useEffect, useState } from 'react';
+import { Alert, Button } from '@heroui/react';
+
+
+export const MyLogoutAlert = () => {
+  const { user } = useAuthentication();
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      setShowAlert(true);
+      setTimeout(() => setShowAlert(false), 5000); // Hide after 5 seconds
+    }
+  }, [user]);
+
+  // Apply the opacity transition to fade in/out
+  const alertClasses = showAlert
+    ? 'opacity-100 transition-opacity duration-500'
+    : 'opacity-0 transition-opacity duration-500';
+
+  return (
+    <div
+      className={`fixed bottom-5 left-5 z-50 w-auto max-w-md ${alertClasses}`}
+    >
+      <Alert
+        color="warning"
+        title="Lejárt munkamenet"
+        description="Jelentkezzen be újra"
+        endContent={
+          <Button color="warning" size="sm" variant="flat">
+            <a href="/login">Bejelentkezés</a>
+          </Button>
+        }
+        variant="faded"
+      />
+    </div>
+  );
+};
