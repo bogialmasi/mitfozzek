7d4593955a21a23f4faee1fda74251078d7d56c1
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-29 13:10:15 +0100

    saving ingredient and recipe as types

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,25 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface Ingredient {
+  id: number;
+  name: string;
+}
+
+export interface Recipe {
+  recipe_id: number;
+  recipe_name: string;
+  recipe_description: string;
+  recipe_time: string;
+  recipe_headcount: number;
+  source_user_id: number;
+  ingredients: Ingredient[];
+}
+
+export interface User {
+  userId: number;
+  username: string;
+  userDescription: string | null;
+  email: string;
+}
