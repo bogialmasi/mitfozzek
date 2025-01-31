141915d4466e797024d8e157c192c330051aada7
bogialmasi <boglarkaalmasi@gmail.com>
2025-01-31 14:00:15 +0100

    setting expiration longer

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load the environment variables from .env.local
 
 const JWT_SECRET = process.env.JWT_SECRET!;  // The secret key for JWT signing, The '!' ensures it won't be undefined
-const EXPIRES = '15m';
+const EXPIRES = '1h';
 // Check if the JWT_SECRET is defined
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET nincs a lokális változók között");
