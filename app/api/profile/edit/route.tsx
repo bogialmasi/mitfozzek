2cc4391ac3ff7b9e876624db512f772f6b692f97
bogialmasi <boglarkaalmasi@gmail.com>
2025-02-03 18:33:20 +0100

    profile editing

---
+++ app/api/profile/edit/route.tsx
@@ -0,0 +1,82 @@
+import pool from '@/lib/db';
+import * as jwt from 'jsonwebtoken';
+import { NextRequest, NextResponse } from 'next/server';
+import { ResultSetHeader } from 'mysql2';
+import argon2 from 'argon2';
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+
+export async function PATCH(req: NextRequest) {
+    try {
+        // Get the token from the Authorization header
+        console.log(req.headers);
+        console.log('Authorization header:', req.headers.get('Authorization'));
+        const authorization = req.headers.get('Authorization');
+        const token = authorization?.split(' ')[1];
+        console.log('Token:', token);
+        if (!token) {
+            return NextResponse.json({ success: false, message: 'Authorization token missing' }, { status: 404 });
+
+        }
+
+        // Verify and decode the token
+        const decoded: any = jwt.verify(token, JWT_SECRET);
+        const userId = decoded.userId; // Get userId from the decoded token
+        console.log("userId:", userId);
+
+        if (!userId) {
+            return NextResponse.json({ success: false, message: 'No userId' }, { status: 401 });
+        }
+
+        const { username, email, description, password } = await req.json();
+
+        let query = `UPDATE users SET `;
+        const conditions = [];
+        const params = [];
+
+        if (password) {
+            const hashedPassword = await argon2.hash(password);
+            conditions.push('password = ?');
+            params.push(hashedPassword);
+        }
+
+        if (username) {
+            conditions.push('username = ?');
+            params.push(username);
+        }
+
+        if (email) {
+            conditions.push('email = ?');
+            params.push(email);
+        }
+
+        if (description) {
+            conditions.push('user_desc = ?');
+            params.push(description);
+        }
+
+        if (conditions.length === 0) {
+            return NextResponse.json({ success: false, message: 'No data to update' }, { status: 400 });
+        }
+
+        query += conditions.join(', ') + " WHERE user_id = ?"
+        params.push(userId);
+        console.log("query:", query);
+        console.log("params:", params);
+
+        const [result, _] = await pool.query(query, params);
+
+        const affectedRows = (result as ResultSetHeader).affectedRows;
+        if (affectedRows === 0) {
+            return NextResponse.json({ success: false, message: 'Profile edit failed' }, { status: 404 });
+        }
+
+        return NextResponse.json({ status: 200 });
+
+    } catch (error) {
+        console.error('Error editing profile:', error);
+        return NextResponse.json({ message: 'Editing profile failed' }, { status: 500 });
+
+    }
+}
--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,12 +1,177 @@
-import { title, subtitle } from "@/components/primitives";
+'use client'
+import { HeroCancel, HeroCheck } from "@/components/icons";
+import { title } from "@/components/primitives";
+import { Button } from "@heroui/button";
+import { Form } from "@heroui/form";
+import { Input, Textarea } from "@heroui/input";
+import { Spinner } from "@heroui/react";
+import { PressEvent } from "@react-types/shared";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function EditProfilePage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+    const token = localStorage.getItem('token');
+    console.log(token);
+    if (!token) {
+      setError('Bejelentkezés szükséges');
+      setLoading(false);
+      return;
+    }
+    const fetchProfile = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('/api/profile', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        if (data.success && data.user_id && data.username) {
+          setUsername(data.username);
+          setEmail(data.email);
+          setDescription(data.user_desc);
+        } else {
+          setError(data.message || 'Edit profile failed');
+        }
+      }
+      catch (err) {
+        setError('Something went wrong.');
+        setLoading(false);
+      }
+      finally {
+        setLoading(false);
+      }
+    }; fetchProfile();
+  }, [])
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    const updatedData: any = {};
+    if (username) updatedData.username = username;
+    if (email) updatedData.email = email;
+    if (description) updatedData.description = description;
+    if (password) updatedData.password = password;
+
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setError('Bejelentkezés szükséges');
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/profile/edit', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(updatedData)
+      });
+
+      if (!response.ok) {
+        setError(`Hiba: ${response.statusText} (${response.status})`);
+        return;
+      }
+
+      const data = await response.json();
+      if (response.status === 200) {
+        setLoading(false);
+        router.push('/profile');
+      } else {
+        setError(data.message || 'Update failed.');
+      }
+    } catch (err) {
+      console.error('Error submitting profile update:', err);
+      setError('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Does not save any of the changes
+  function handleCancel(e: PressEvent): void {
+    router.push('/profile');
+  }
+
+  if (loading) return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <p>Betöltés...</p>
+        <Spinner />
+      </div>
+    </div>);
+
+  if (error) return <div>{error}</div>;
+
+
+
   return (
     <div>
       <h1 className={title()}>Profil módosítása</h1>
-      <div className={subtitle({ class: "mt-4" })}>
-        Saját profil módosítása, törlése
-      </div>
+      <Form onSubmit={handleSubmit} validationBehavior="native"
+        className="w-full flex flex-col gap-6 py-4">
+        <Input
+          value={username} onChange={(e) => setUsername(e.target.value)}
+          errorMessage="!"
+          label="Felhasználónév"
+          labelPlacement="outside"
+          name="username"
+          placeholder="Felhasználónév"
+          type="text"
+          variant="bordered"
+        />
+        <Input
+          value={password} onChange={(e) => setPassword(e.target.value)}
+          errorMessage="!"
+          label="Jelszó"
+          labelPlacement="outside"
+          name="password"
+          placeholder="Jelszó"
+          type="password"
+          variant="bordered"
+        />
+        <Input
+          value={email} onChange={(e) => setEmail(e.target.value)}
+          errorMessage="Adjon meg érvényes emailt"
+          label="Email"
+          labelPlacement="outside"
+          name="email"
+          placeholder="Email"
+          type="email"
+          variant="bordered"
+        />
+        <Textarea
+          value={description} onChange={(e) => setDescription(e.target.value)}
+          labelPlacement="outside"
+          name="description"
+          placeholder="Magamról"
+          type="text"
+          variant="bordered"
+          maxLength={250}
+          errorMessage="Maximum 250 karakter megengedett"
+          isClearable
+          onClear={() => setDescription('')}
+          isInvalid={description.length >= 250}
+        />
+        <div className="py-4 flex justify-center w-full space-x-4">
+          <Button type="submit"><HeroCheck />Módosítások mentése</Button><Button type="button" onPress={handleCancel}><HeroCancel />Mégsem</Button>
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </Form>
     </div>
   );
 }
