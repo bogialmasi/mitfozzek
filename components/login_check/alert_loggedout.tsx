bf7c44aac4ea9b7550f92cdfbe86fd94d71bd67b
bogialmasi <boglarkaalmasi@gmail.com>
2025-02-06 16:29:46 +0100

    alert doesnt show when there was no previous login

--- components/login_check/alert_loggedout.tsx
+++ components/login_check/alert_loggedout.tsx
@@ -6,16 +6,20 @@ import { Alert, Button } from '@heroui/react';
 
 
 export const MyLogoutAlert = () => {
-  const { user } = useAuthentication();
+  const { user, logout } = useAuthentication();
   const [showAlert, setShowAlert] = useState(false);
-
+  
   useEffect(() => {
-    if (!user) {
+    const session = localStorage.getItem('session');
+    if (session === 'true' && user === null) { // 'true' is string, user is logged out
+      console.log("User got logged out");
       setShowAlert(true);
-      setTimeout(() => setShowAlert(false), 5000); // Hide after 5 seconds
+      setTimeout(() => setShowAlert(false), 5000); // Alert shown for 5 sec
+      logout(); // Clear session and user state
     }
-  }, [user]);
+  }, [user, logout]);
 
+  
   // Apply the opacity transition to fade in/out
   const alertClasses = showAlert
     ? 'opacity-100 transition-opacity duration-500'
@@ -39,3 +43,35 @@ export const MyLogoutAlert = () => {
     </div>
   );
 };
+  /*
+    useEffect(() => {
+      //const token = localStorage.getItem('token');
+      const session = localStorage.getItem('session');
+      console.log('SESSION AFTER LOGIN:', session);
+      if (session === 'true') {
+        setWasSession(true);
+      }
+      setValidatedSession(true);
+    }, [user]);*/
+/*
+  useEffect(() => {
+    if (validatedSession && !checkedSession) {
+      setCheckedSession(true); // session is checked
+    }
+  }, [validatedSession]);
+
+  useEffect(() => {
+    if (validatedSession && wasSession && user === null && checkedSession) {
+      console.log('validated: ', validatedSession, ',was session: ', wasSession, ',user: ', user)
+      console.log("User got logged out")
+
+      const session = localStorage.getItem('session');
+      logout();
+      console.log('SESSION AFTER LOGOUT:', session)
+
+
+      setShowAlert(true);
+      setTimeout(() => setShowAlert(false), 5000); // Hide after 5 seconds
+    }
+  }, [user, wasSession, validatedSession, logout, checkedSession]);
+*/
