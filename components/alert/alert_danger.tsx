50e4594843cf44209392b7ef3384eea2f06446c8
bogialmasi <boglarkaalmasi@gmail.com>
2025-02-12 13:07:29 +0100

    alerts placed in different component folder

---
+++ components/alert/alert_danger.tsx
@@ -0,0 +1,33 @@
+import { Alert } from "@heroui/react";
+import { useEffect, useState } from "react";
+
+interface MyDangerAlertProps {
+    title: string;
+    description: string;
+}
+
+export const MyDangerAlert: React.FC<MyDangerAlertProps> = ({ title, description}) => {
+    const [showAlert, setShowAlert] = useState(false);
+
+    useEffect(() => {
+        setShowAlert(true);
+        // Hide the alert after 2 sec
+        const timer = setTimeout(() => {
+            setShowAlert(false);
+        }, 2000);
+        return () => clearTimeout(timer); // Cleanup the timeout on unmount
+    }, []);
+
+    // Apply the opacity transition to fade in/out
+    const alertClasses = showAlert
+        ? 'opacity-100 transition-opacity duration-500'
+        : 'opacity-0 transition-opacity duration-500';
+
+    return (
+        <div className={`fixed bottom-5 left-5 z-50 w-auto max-w-md ${alertClasses}`}>
+            <div className="flex items-center justify-center w-full">
+                <Alert description={description} title={title} color="danger" />
+            </div>
+        </div>
+    );
+}
