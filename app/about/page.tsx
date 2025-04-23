199365dd94b4adeb36ecba8937aff0b40a1c6883
bogialmasi <boglarkaalmasi@gmail.com>
2025-04-23 19:08:19 +0200

    about description

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,9 @@ export default function AboutPage() {
     <div>
       <h1 className={title()}>Mit főzzek?</h1>
       <div className={subtitle({ class: "mt-4" })}>
-        Oldal leírása
+        A mitfőzzek célja a mindennapok megkönnyítése azzal, hogy az otthonunkban található összetevők alapján az igényeinknek megfelelő fogásokat készíthettünk.
+        Emellett a spájzunk nyilvántartására, a bevásárlólisták írására, valamint a "meal planning" étrendtervezésre is megoldást nyújt.
+        Használatával csökken az ételek pazarlása, így a környezetünkre is jótékony hatással van.
       </div>
     </div>
   );
