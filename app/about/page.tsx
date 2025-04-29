602a1298138be73ae318aeadbe4c40d1ea32b0f0
bogialmasi <boglarkaalmasi@gmail.com>
2025-04-29 14:39:59 +0200

    "

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
       <h1 className={title()}>Mit főzzek?</h1>
       <div className={subtitle({ class: "mt-4" })}>
         A mitfőzzek célja a mindennapok megkönnyítése azzal, hogy az otthonunkban található összetevők alapján az igényeinknek megfelelő fogásokat készíthettünk.
-        Emellett a spájzunk nyilvántartására, a bevásárlólisták írására, valamint a "meal planning" étrendtervezésre is megoldást nyújt.
+        Emellett a spájzunk nyilvántartására, a bevásárlólisták írására, valamint a meal planning étrendtervezésre is megoldást nyújt.
         Használatával csökken az ételek pazarlása, így a környezetünkre is jótékony hatással van.
       </div>
     </div>
